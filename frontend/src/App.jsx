4f8166c10e6e67b91f13958eddf3a0f77180591e
Anant Singh <anantprataps857@gmail.com>
2026-01-07 15:09:20 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
 
   const [showLogin,setShowLogin] = useState(false)
 
-  return 
+  return (
     <>
     {showLogin?<LoginPop setShowLogin={setShowLogin}/>:<></>}
        <div className='app'>
